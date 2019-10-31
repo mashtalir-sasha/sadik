ff3a5bc54ebf70fec4dd3be2452d51675b8f64bd
mashtalir-sahsa <mashtalir_sasha@ukr.net>
2019-11-01 01:03:45 +0200

    1.0.0 ver

--- app/mail.php
+++ app/mail.php
@@ -12,7 +12,7 @@
 	if(isset ($_POST['step3'])) {$step3=$_POST['step3'];}
 	if(isset ($_POST['step4'])) {$step4=$_POST['step4'];}
 
-	$to = "mashtalir_sasha@ukr.net"; // Замениь на емаил клиента
+	$to = "d.o.dikaya@gmail.com"; // Замениь на емаил клиента
 
 	$message = "Форма: $title <br><br>";
 	if ( $fio || $phonenum || $step1 || $checkbox || $step3 || $step4 ) {
@@ -20,17 +20,17 @@
 			. ( $fio ?" Імя:  $fio <br>" : "")
 			. ( $phonenum ?" Телефон:  $phonenum <br><br>" : "")
 			. ( $step1  ? " Вкажіть дату народження Вашої дитини?: $step1 <br>" : "")
-			. ( $checkbox  ? " На що Ви більше всього звертаєте увагу при виборі навчального закладу для Вашої дитини?: $checkbox <br>" : "")
+			. ( $checkbox  ? "На що Ви більше всього звертаєте увагу при виборі навчального закладу для Вашої дитини?: $checkbox <br>" : "")
 			. ( $step3  ? " Ваша дитина вже ходила в дитячий садочок?: $step3 <br>" : "")
 			. ( $step4  ? " Який графік перебування у дитячому садку Вам зручніше і більше підійде для дитини?: $step4 <br>" : "");
 	}
 
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-type: text/html; charset=UTF-8\r\n";
-	$headers .= "From: no-reply@site.com"; // Заменить домен на домен клиента
+	$headers .= "From: no-reply@mywaypreschool.com"; // Заменить домен на домен клиента
 
 	if (!$title && !$phonenum) {
 	} else {
-		mail($to,"New lead(site.com)",$message,$headers); // Заменить домен на домен клиента
+		mail($to,"New lead(quiz.mywaypreschool.com)",$message,$headers); // Заменить домен на домен клиента
 	}
 ?>
